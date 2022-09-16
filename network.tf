d1f3926790f7c532d4338521714093545846fd83
simkibo12 <simkibo12@naver.com>
2022-09-16 05:09:49 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/simkibo/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "testtg"
+  resource_group_name = "simkibo-workshop"
   # insert required variables here
 }
